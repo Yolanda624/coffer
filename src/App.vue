7b6f70fc663e41e128353fb9dbefab94ea7be214
Yolanda624 <1416297612@qq.com>
2020-08-06 14:04:59 +0800

    添加精灵球

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="nav"></div>
+    <router-view />
+
+    <div class="call-wrapper">
+      <div class="diggit"></div>
+      <div class="clear"></div>
+      <div class="diggword">我不负责赚钱养家哦~~</div>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   // text-align: center;
   color: #2c3e50;
+}
+.call-wrapper {
+  position: fixed;
+  bottom: 50px;
+  margin: 0px;
+  right: 16px;
+  animation: jumping 5s ease-in-out;
+  animation-iteration-count: infinite;
+  float: right;
+  font-size: 12px;
+  width: 125px;
+  text-align: center;
+  .diggit {
+    background: url('~@/assets/ball.png') no-repeat;
+    border-radius: 50%;
+    box-shadow: 0px 0px 15px 5px #fff inset;
+    cursor: pointer;
+    height: 100px;
+    margin: 0px;
+    padding: 0px;
+    width: 100px;
+    text-align: center;
+    &::before {
+      content: "主人负责貌美如花";
+      position: absolute;
+      top: -40px;
+      left: 7px;
+      font-size: 14px;
+      color: #5bf;
+    }
+  }
+  .diggword {
+    color: red;
+    margin-top: 20px;
+  }
+  @keyframes jumping {
+    0% {
+      transform: translateY(0px);
+    }
+    50% {
+      transform: translateY(-100px);
+    }
+    100% {
+      transform: translateY(0px);
+    }
+  }
 }
 </style>
